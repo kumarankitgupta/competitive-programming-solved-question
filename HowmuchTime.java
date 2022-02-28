0a40bd6a5bbdf3c844cae2ac7af2515cf0e9edfa
kumarankitgupta <ankitkrgupta149@gmail.com>
2022-02-28 19:18:56 +0530

    changes

--- HowmuchTime.java
+++ HowmuchTime.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class HowmuchTime {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int t = sc.nextInt();
+            int time = sc.nextInt();
             while(time-->0){
                 int n = sc.nextInt();
                 int p = sc.nextInt();
